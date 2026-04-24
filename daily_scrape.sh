020a3a26fbb1e0261d5e760fc38ad4894bdab30e
Gabriel Guzman <gabe.r.guzman@gmail.com>
2026-04-24 16:20:23 -0500

    quick commit

--- daily_scrape.sh
+++ daily_scrape.sh
@@ -3,13 +3,17 @@ jupyter nbconvert --to script get_min.ipynb
 jupyter nbconvert --to script cluster/cluster2_scrape.ipynb
 jupyter nbconvert --to script distance/set_distance.ipynb
 #jupyter nbconvert --to script roles.ipynb
-#jupyter nbconvert --to script gen_opp.ipynb
+jupyter nbconvert --to script gen_opp.ipynb
 
 
 python get_min.py
 python cluster/cluster2_scrape.py  
 
 python distance/set_distance.py
+python gen_opp.py
+jupyter nbconvert --to script series set_series_improved.ipynb
 
+python series/set_series_improved.py
+cp series/series_index.csv ../../web_app/data/series_index.csv
 #python roles.py
 #python gen_opp.py
